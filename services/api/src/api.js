5e9fffb19c7aad1d94fdb32c5ee2b9d0f530df53
Iheb KHEMISSI <iheb.khemissi+github@gmail.com>
2024-06-14 21:30:12 +0000

    feat: Create a simple API to be deployed using Functions
    Create a simple API to be deployed to Azure Functions (compatible with the "Functions" ADE environment).
    The QnA API will be used to demonstrate the integration of AZD and ADE via a configurable logic which makes use of "iterations": by defualt we will start with RELEASE=1, and by increasing the release number (to simulate a change) we will get a different behavior which can be increased errors, more latency, or no issues at all.

--- services/api/src/api.js
+++ services/api/src/api.js
@@ -1,35 +1,46 @@
 const crypto = require('crypto');
-const { app, output } = require('@azure/functions');
+const { setTimeout } = require('node:timers/promises');
+const { app } = require('@azure/functions');
 
-const serviceBusOutput = output.serviceBusQueue({
-    queueName: '%SERVICEBUS_QUEUE%',
-    connection: 'SB_ORDERS',
-});
+const RELEASE = +process.env.RELEASE || 1;
 
-app.http('api', {
+app.http('qna', {
     methods: ['POST'],
     authLevel: 'anonymous',
-    return: serviceBusOutput,
     handler: async (request, context) => {
         const id = crypto.randomUUID();
-        context.log(`Processing order ${id}`);
 
-        const order = await request.json();
+        context.log(`Processing request ${id}`);
+        const payload = await request.json();
 
-        // Simple validation: ensure the order has items
-        if (!Array.isArray(order.items) || !order.items.length) {
-            throw new Error('Order must have at least one item');
+        // Simple payload validation
+        if (!payload.question) {
+            throw new Error('Invalid payload: missing question');
         }
 
-        // // Generate some random errors for testing
-        // if (Math.random() < 0.1) {
-        //     throw new Error('Random error');
-        // }
+        // Release 2 introduces some errors
+        if (RELEASE === 2) {
+            // Generate some random errors for testing
+            if (Math.random() < 0.1) {
+                throw new Error('Random error');
+            }
+        }
+
+        // Release 3 introduces some latency
+        if (RELEASE === 4) {
+            await setTimeout(2000); // wait 2 seconds
+        }
 
         return {
-            id,
-            createdAt: new Date().toISOString(),
-            ...order,
+            jsonBody: {
+                id,
+                question: payload.question,
+                answer: '42',
+                createdAt: new Date().toISOString(),
+            },
+            headers: {
+                'X-Release': RELEASE,
+            },
         };
     }
 });
